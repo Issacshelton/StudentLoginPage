bdf82173d89a9f6990a644bba9a4387eb276c220
Issacshelton <52657414+Issacshelton@users.noreply.github.com>
2023-04-03 11:51:47 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+function Save(){
+    const fname = document.getElementById(fname);
+    const lname = document.getElementById(lname);
+    const emailid = document.getElementById(emailid);
+    const phno = document.getElementById(phno);
+    const dob = document.getElementById(dob);
+    const gender = document.getElementById(gender);
+    const adl1 = document.getElementById(adl1);
+    const adl2 = document.getElementById(adl2);
+    const city = document.getElementById(city);
+    const state = document.getElementById(state);
+    const postalcode = document.getElementById(postalcode);
+
+}
